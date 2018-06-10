de498d20caaf8dfb411d8f5baff7a473792c7724
TaniaNal <32977909+TaniaNal@users.noreply.github.com>
2018-06-10 21:46:20 +0300

    Add files via upload

---
+++ Laba1/src/Laba1/Main.java
@@ -0,0 +1,30 @@
+package Laba1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Action action1 = new Action();
+		Action action2 = new Action("VOC", 2017, 201, 1);
+		Action action3 = new Action("Security", 2006, 1256, 75, 452);
+		
+		System.out.println(action1.toString());
+		System.out.println(action2.toString());
+		System.out.println(action3.toString());
+		
+		Action.printStaticSum();
+		action3.printSum();
+		
+		action1.resetValues("Love", 2018, 653, 10, 47);
+		action3.resetValues("Security", 2007, 45, 202, 758);
+		action2.setPrice(1255);
+		
+		System.out.println(action1.toString());
+		System.out.println(action2.toString());
+		System.out.println(action3.toString());
+		
+		Action.printStaticSum();
+		action1.printSum();
+
+	}
+
+}
